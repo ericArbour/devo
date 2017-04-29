21b4aca04bbf8534c2c7e2d2e4d324e0f1ef2194
Eric Arbour <arboureric@gmail.com>
2017-04-29 12:15:56 -0400

    complete, but no music due

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-var audio = new Audio('dev04.mp3');
-audio.addEventListener('ended', function() {
-    this.currentTime = 0;
-    this.play();
-}, false);
-audio.autoplay = true;
-audio.play();
+// var audio = new Audio('dev04.mp3');
+// audio.addEventListener('ended', function() {
+//     this.currentTime = 0;
+//     this.play();
+// }, false);
+// audio.autoplay = true;
+// audio.play();
+
+//excluded due to potential copyright issues with sound clip
